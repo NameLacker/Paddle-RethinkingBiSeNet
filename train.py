46bba995d2330c9b20d16a451e6ac55d3332d6d0
shutao <shutao@ktwlrj.com>
2021-08-12 12:30:36 +0800

    项目更新

--- train.py
+++ train.py
@@ -11,12 +11,15 @@
 """
 
 import numpy as np
+import logging
 import paddle
+import time
 import os
 
 from paddle import optimizer
 import paddle.nn.functional as F
 from paddle.io import DataLoader
+from visualdl import LogWriter
 
 from tools.data_reader import CityScapes
 from models.network import BiSeNet
@@ -24,6 +27,37 @@ from models.network import BiSeNet
 from tools.utils import get_configuration
 
 cfg = get_configuration()
+log_name = str(int(time.time()))
+log_writer = LogWriter("log/train_" + log_name)
+logger = logging.getLogger()
+
+
+def init_log_config():
+    """
+    初始化日志相关配置
+    :return:
+    """
+    global logger
+    # 设置日志级别
+    logger.setLevel(logging.DEBUG)  # 设置日志级别
+    # 日志文件保存配置
+    log_path = os.path.join(os.getcwd(), 'logs')
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    log_name = os.path.join(log_path, 'train_' + str(int(time.time())) + '.log')
+    fh = logging.FileHandler(log_name, mode='w')
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+    fh.setFormatter(formatter)
+    # 日志控制台输出配置
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+    # 添加配置
+    logger.addHandler(streamHandler)
+    logger.addHandler(fh)
+
+
+init_log_config()  # 初始化日志输出
 
 
 def run_train():
@@ -55,6 +89,9 @@ def run_train():
     opt = optimizer.SGD(learning_rate=scheduler, parameters=net.parameters(), weight_decay=5e-4)
 
     maxmIOU50 = -1
+    stop_count = 0
+    train_step = 0
+    val_step = 0
     for epoch_id in range(train_cfg["num_epochs"]):
         net.train()
         for batch_id, (image, label) in enumerate(train_reader):
@@ -74,18 +111,18 @@ def run_train():
             boundery_bce_loss, boundery_dice_loss = 0., 0.
             if use_boundary_2:
                 # detail2 损失
-                boundery_bce_loss2,  boundery_dice_loss2 = net.boundary_loss(detail2, label)
+                boundery_bce_loss2, boundery_dice_loss2 = net.boundary_loss(detail2, label)
                 boundery_bce_loss += boundery_bce_loss2
                 boundery_dice_loss += boundery_dice_loss2
 
             if use_boundary_4:
-                boundery_bce_loss4,  boundery_dice_loss4 = net.boundary_loss(detail4, label)
+                boundery_bce_loss4, boundery_dice_loss4 = net.boundary_loss(detail4, label)
                 boundery_bce_loss += boundery_bce_loss4
                 boundery_dice_loss += boundery_dice_loss4
 
             if use_boundary_8:
                 # detail8 损失
-                boundery_bce_loss8,  boundery_dice_loss8 = net.boundary_loss(detail8, label)
+                boundery_bce_loss8, boundery_dice_loss8 = net.boundary_loss(detail8, label)
                 boundery_bce_loss += boundery_bce_loss8
                 boundery_dice_loss += boundery_dice_loss8
 
@@ -93,13 +130,16 @@ def run_train():
             loss.backward()
             opt.minimize(loss)
             net.clear_gradients()
-            print("Epoch: {}, Batch: {}, "
-                  "Loss: {:.4}, boundery_bce_loss: {:.4}, boundery_dice_loss: {:.4}, "
-                  "learning_rate: {}"
-                  .format(epoch_id, batch_id,
-                          loss.numpy()[0], boundery_bce_loss.numpy()[0], boundery_dice_loss.numpy()[0],
-                          scheduler.get_lr()))
-
+            log_writer.add_scalar("train/loss", step=train_step, value=loss.numpy()[0])
+            log_writer.add_scalar("train/boundery_bce_loss", step=train_step, value=boundery_bce_loss.numpy()[0])
+            log_writer.add_scalar("train/boundery_dice_loss", step=train_step, value=boundery_dice_loss.numpy()[0])
+            train_step += 1
+            logger.info("Epoch: {}, Batch: {}, "
+                        "Loss: {:.4}, boundery_bce_loss: {:.4}, boundery_dice_loss: {:.4}, "
+                        "learning_rate: {}"
+                        .format(epoch_id, batch_id,
+                                loss.numpy()[0], boundery_bce_loss.numpy()[0], boundery_dice_loss.numpy()[0],
+                                scheduler.get_lr()))
         net.eval()
         hist = np.zeros((n_classes, n_classes), dtype=np.float32)
         for batch_id, (image, label) in enumerate(test_reader):
@@ -122,13 +162,21 @@ def run_train():
 
         ious = np.diag(hist) / (np.sum(hist, axis=0) + np.sum(hist, axis=1) - np.diag(hist))
         mIOU50 = np.mean(ious)
-        print(mIOU50)
+        log_writer.add_scalar("eval/mIOU50", step=val_step, value=mIOU50)
+        val_step += 1
+        logger.info("当前验证集平均 mIOU50: {}".format(mIOU50))
         if mIOU50 >= maxmIOU50:
+            stop_count = 0
             maxmIOU50 = mIOU50
-            save_pth = os.path.join(train_cfg["params_dir"], "model_maxmIOU50_{:.2}.pdparams".format(maxmIOU50*100))
-
+            save_pth = os.path.join(train_cfg["params_dir"], "model_maxmIOU50_{:.2}.pdparams".format(maxmIOU50 * 100))
             model = net.state_dict()
             paddle.save(model, save_pth)
+            logger.info("成功保存模型参数...")
+        else:
+            stop_count += 1
+        if stop_count == 5:
+            logger.info("提前停止训练")
+            exit(0)
         scheduler.step()  # 更新学习率
 
 
